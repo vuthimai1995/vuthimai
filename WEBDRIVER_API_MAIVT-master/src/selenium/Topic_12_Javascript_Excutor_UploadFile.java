8125d94038585495d2a0cd621e5bc6a3ad043910
Mai VT <Mai VT@10.3.2.49>
2019-07-10 14:33:57 +0700

    commit topic_12

--- WEBDRIVER_API_MAIVT-master/src/selenium/Topic_12_Javascript_Excutor_UploadFile.java
+++ WEBDRIVER_API_MAIVT-master/src/selenium/Topic_12_Javascript_Excutor_UploadFile.java
@@ -1,49 +1,56 @@
 package selenium;
 
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import com.sun.corba.se.spi.orbutil.fsm.Action;
-import com.sun.glass.ui.Size;
 
-import sun.misc.PerformanceLogger;
 
+
+
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
+
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.thoughtworks.selenium.webdriven.commands.KeyEvent;
 
 public class Topic_12_Javascript_Excutor_UploadFile {
 	WebDriver driver;
-	Actions action;
-	Alert alert;
 	String randomEmail;
 	String root_Folder_Path, image_01_Path, image_02_Path, image_03_Path;
 	String image_01_Name = "v1.jpg";
 	String image_02_Name = "v2.jpg";
 	String image_03_Name = "v3.jpg";
+	String chromePath;
+	String firefoxPath;
 	
 	@BeforeClass
 	public void beforeClass() {
 		//driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver = new FirefoxDriver();
-		action = new Actions(driver);
+		//driver = new FirefoxDriver();
 		// sau khi mo trang guru thi phai nhap ham doi
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		randomEmail = "vuthanhmai" + randomData() + "@gmail.com";
 		root_Folder_Path = System.getProperty("user.dir");
 		System.out.println(root_Folder_Path);
+		chromePath = root_Folder_Path + "\\uploadFile\\chrome.exe" ;
+		firefoxPath = root_Folder_Path + "\\uploadFile\\firefox.exe" ;
+		image_01_Path = root_Folder_Path + "\\UploadFile\\" + image_01_Name;
+		image_02_Path = root_Folder_Path + "\\UploadFile\\" + image_02_Name;
+		image_03_Path = root_Folder_Path + "\\UploadFile\\" + image_03_Name;
 	}
 
 	public int randomData() {
@@ -136,14 +143,100 @@ public class Topic_12_Javascript_Excutor_UploadFile {
 		//step 9: veryfy du lieu co hien thi voi 1 xpath
 		WebElement wishlisttext = driver.findElement(By.xpath("//th[text()='WISHLIST_CNT']//following-sibling::td[text()='The number of items in your Wishlist.']"));
 		Assert.assertTrue(wishlisttext.isDisplayed());
-		
+		  
 		//step 10: navigative toi http://demo.guru99.com/v4/
 		
 	}
 	
-
 	
+		//@Test
+		public void TC02_UploadFile_Senkeys() throws Exception {
+			driver.get("http://blueimp.github.io/jQuery-File-Upload/");
+			//Step 2: senkeys
+			WebElement uploadFile = driver.findElement(By.xpath("//input[@type='file']"));
+			
+			//su ly duong dan tuong doi de chay qua may khac
+			//uploadfile nhieu file 1 luc
+			uploadFile.sendKeys(image_01_Path+"\n" + image_02_Path+"\n" + image_03_Path);
+			//Doi 3 file load success
+			List <WebElement> fileload = driver.findElements(By.xpath("//tbody[@class='files']//p[@class='name']"));
+			for (WebElement file: fileload) {
+				System.out.println(file.getText());
+				Assert.assertTrue(file.isDisplayed());
+			}
+			
+			//Step 4: click start button de upload cho ca 3 file
+			List <WebElement> startbutton = driver.findElements(By.xpath("//tbody[@class='files']//button[@class='btn btn-primary start']"));
+			for(WebElement start: startbutton) {
+				//click bang javascript do bi che quang cao
+				clickToElementByJs(start);
+				Thread.sleep(2000);
+			}
+			
+			//step 5: veryfy 3 file da upload
+			List <WebElement> fileuploaded = driver.findElements(By.xpath("//tbody[@class='files']//p[@class='name']/a"));
+			for(WebElement fileupload: fileuploaded) {
+				Assert.assertTrue(fileupload.isDisplayed());
+			}
+			Assert.assertTrue(driver.findElement(By.xpath("//tbody[@class='files']//a[text()='v1.jpg']")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//tbody[@class='files']//a[text()='v2.jpg']")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//tbody[@class='files']//a[text()='v3.jpg']")).isDisplayed());
+		}
 		
+		//@Test
+		public void TC03_UploadFile_AutoIT() throws Exception {
+			driver.get("http://blueimp.github.com/jQuery-File-Upload/");
+			//Step 2: su dung AutoIT de upload file chay cho 2 trinh duỵet
+			WebElement addFileButton = driver.findElement(By.cssSelector(".fileinput-button"));
+			//click bang javascript do bi che quang cao
+			addFileButton.click();
+			//clickToElementByJs(addFileButton);
+			Thread.sleep(2000);
+			if(driver.toString().contains("chrome")) {
+				Runtime.getRuntime().exec(new String[] {chromePath, image_01_Path});
+			}
+				Runtime.getRuntime().exec(new String[] {firefoxPath, image_01_Path});
+			Assert.assertTrue(driver.findElement(By.xpath("//tbody[@class='files']//p[@class='name' and text()='"+ image_01_Name+ "']")).isDisplayed());
+			Thread.sleep(2000);
+		}
+		//copy 2 file chrome.exe và firefox.exe vào mục uploadfile
+
+		@Test
+		public void TC04_UploadFile_AutoIT() throws Exception {
+			driver.get("http://blueimp.github.com/jQuery-File-Upload/");
+			//Step 2: su dung AutoIT de upload file chay cho 2 trinh duỵet
+			WebElement addFileButton = driver.findElement(By.cssSelector(".fileinput-button"));
+			addFileButton.click();
+			//clickToElementByJs(addFileButton);
+			Thread.sleep(2000);
+			
+			StringSelection select = new StringSelection(image_01_Path);
+			//copy clipboard
+			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(select, null);
+			
+			//thao tac voi desktop app qua lib awt
+			Robot robot = new Robot();
+			Thread.sleep(2000);
+			
+			//nhan phim enter (focus chuot va input file)
+			robot.keyPress(java.awt.event.KeyEvent.VK_ENTER);
+			robot.keyRelease(java.awt.event.KeyEvent.VK_ENTER);
+			
+			//Nhan ctrl V
+			robot.keyPress(java.awt.event.KeyEvent.VK_CONTROL);
+			robot.keyPress(java.awt.event.KeyEvent.VK_V);
+			
+			//Nha ctrl V
+			robot.keyRelease(java.awt.event.KeyEvent.VK_CONTROL);
+			robot.keyRelease(java.awt.event.KeyEvent.VK_V);
+			
+			//Nhan enter (open file)
+			robot.keyPress(java.awt.event.KeyEvent.VK_ENTER);
+			robot.keyRelease(java.awt.event.KeyEvent.VK_ENTER);
+			
+			Assert.assertTrue(driver.findElement(By.xpath("//tbody[@class='files']//p[@class='name' and text()='"+ image_01_Name+ "']")).isDisplayed());
+			Thread.sleep(2000);
+		}
 		
 	// @AfterClass
 	public void afterClass() {
